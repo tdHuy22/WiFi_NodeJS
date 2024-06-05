998a0ad7662ed51d30fc221b2b586d681c1c5219
tdHuy22 <20520550@gm.uit.edu.vn>
2024-06-05 10:14:11 +0700

    systemctl -> service

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { exec } = require("child_process");
 const {
   startInternetCheck,
-  turnOnAccessPoint,
   checkInternetConnection,
   getIpAddress,
 } = require("./src/middleware/internet");
+const { turnOnAccessPoint } = require("./src/middleware/accessPoint");
 const path = require("path");
 const bodyParser = require("body-parser");
 const router = require("./src/route/route");
@@ -22,21 +22,21 @@ app.use(express.static(path.join(__dirname, "/src/public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/src/view"));
-app.use(express.static(path.join(__dirname, "/src/public")));
 
 app.use("/", router);
-app.use((err, res, req, next) => {
+app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something went wrong!");
+  res.status(500);
+  res.send("Something went wrong!");
 });
 
-app.listen(PORT, getIpAddress(), () => {
-  console.log(`Server is running on http://${getIpAddress()}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
   if (checkInternetConnection()) {
     console.log("Internet is connected.");
-    exec(
-      "chromium-browser --kiosk --enable-browser-cloud-management https://192.168.1.5:8000/screen"
-    );
+    // exec(
+    //   "chromium-browser --kiosk --enable-browser-cloud-management https://192.168.1.5:8000/screen"
+    // );
   } else {
     turnOnAccessPoint();
     startInternetCheck();
--- src/middleware/accessPoint.js
+++ src/middleware/accessPoint.js
@@ -3,8 +3,8 @@ const { execSync } = require("child_process");
 function turnOnAccessPoint() {
   try {
     console.log("Turning on access point...");
-    execSync("sudo systemctl stop dhcpcd");
-    execSync("sudo systemctl start NetworkManager");
+    execSync("sudo service dhcpcd stop");
+    execSync("sudo service NetworkManager start");
     // execSync(
     //   "sudo nmcli device wifi hotspot ifname wlan0 con-name UTK_Converter ssid RPI_Zero password RPI012345"
     // );
@@ -13,6 +13,8 @@ function turnOnAccessPoint() {
   } catch (error) {
     console.error("Error turning on access point:", error);
     execSync("sudo systemctl restart NetworkManager");
+    const stdoutErr = execSync("sudo nmcli device connect wlan0");
+    console.log(stdout.toString());
   }
 }
 
@@ -21,8 +23,8 @@ function turnOffAccessPoint() {
     console.log("Turning off access point...");
     // execSync("sudo nmcli connection down UTK_Converter");
     execSync("sudo nmcli device disconnect wlan0");
-    execSync("sudo systemctl stop NetworkManager");
-    const stdout = execSync("sudo systemctl start dhcpcd");
+    execSync("sudo service NetworkManager stop");
+    const stdout = execSync("sudo service dhcpcd start");
     console.log(stdout.toString());
   } catch (error) {
     console.error("Error turning off access point:", error);
